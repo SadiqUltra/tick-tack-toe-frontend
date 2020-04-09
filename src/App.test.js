c16b102d91b456d64dbfa2585984acd7cf25ade0
sadikultra <sadikultra@gmail.com>
2020-04-09 17:48:13 +0600

    Added: Test for Game Engine

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import {checkWin} from './GameEngine';
+import { x,o } from "./constants";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('Win: X', () => {
+  expect(checkWin([
+    [x,  o,    ""],
+    [x,  o,    ""],
+    [x,  "", ""],
+  ])).toBe(x);
 });
+
+test('Win: false', () => {
+  expect(checkWin([
+    [x,   o,    x],
+    [x,   o,    ""],
+    ["",  "", ""],
+  ])).toBe(false);
+});
+
+
+test('Win: O', () => {
+  expect(checkWin([
+    [x,  o,    o],
+    [x,  o,    ""],
+    [o,  "", ""],
+  ])).toBe(o);
+});
+
+
+test('Win: X', () => {
+  expect(checkWin([
+    [x,  o,    o],
+    [x,  x,    ""],
+    [o,  "",   x],
+  ])).toBe(x);
+});
+
+
+test('Win: X', () => {
+  expect(checkWin([
+    [x,  o,   o],
+    [x,  x,   x],
+    [o,  "",  o],
+  ])).toBe(x);
+});
+
+test('Win: false', () => {
+  expect(checkWin([
+    [x,  o,   o],
+    [o,  x,   x],
+    [o,  x,  o],
+  ])).toBe(false);
+});
+
